dbf30423b09d10294fa17e1011d421907b295983
armando0405 <armandomorenoalvarez5@gnail.com>
2025-07-28 20:24:22 -0500

    style: finalizacion de diseño de panel principal

--- src/ventanas/VentanaPrincipal.java
+++ src/ventanas/VentanaPrincipal.java
@@ -29,6 +29,13 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         b_email = new javax.swing.JButton();
         b_volver = new javax.swing.JButton();
         l_fondo = new javax.swing.JLabel();
+        menubar = new javax.swing.JMenuBar();
+        m_opciones = new javax.swing.JMenu();
+        opcion_abrir = new javax.swing.JMenuItem();
+        opcion_guardar = new javax.swing.JMenuItem();
+        opcion_salir = new javax.swing.JMenuItem();
+        m_ayuda = new javax.swing.JMenu();
+        acercade = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -51,7 +58,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
                 b_clientesActionPerformed(evt);
             }
         });
-        getContentPane().add(b_clientes, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 280, 200, 80));
+        getContentPane().add(b_clientes, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 220, 200, 80));
 
         b_productos.setBackground(new java.awt.Color(25, 153, 0));
         b_productos.setFont(new java.awt.Font("Segoe UI", 1, 22)); // NOI18N
@@ -64,7 +71,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
                 b_productosActionPerformed(evt);
             }
         });
-        getContentPane().add(b_productos, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 280, 200, 80));
+        getContentPane().add(b_productos, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 220, 200, 80));
 
         b_ventas.setBackground(new java.awt.Color(25, 153, 0));
         b_ventas.setFont(new java.awt.Font("Segoe UI", 1, 22)); // NOI18N
@@ -72,7 +79,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         b_ventas.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ventanas/ventas.png"))); // NOI18N
         b_ventas.setText("Ventas");
         b_ventas.setBorder(null);
-        getContentPane().add(b_ventas, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 280, 200, 80));
+        getContentPane().add(b_ventas, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 220, 200, 80));
 
         b_almacen.setBackground(new java.awt.Color(25, 153, 0));
         b_almacen.setFont(new java.awt.Font("Segoe UI", 1, 22)); // NOI18N
@@ -80,7 +87,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         b_almacen.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ventanas/almacen.png"))); // NOI18N
         b_almacen.setText("Almacen");
         b_almacen.setBorder(null);
-        getContentPane().add(b_almacen, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 280, 200, 80));
+        getContentPane().add(b_almacen, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 220, 200, 80));
 
         b_salir.setBackground(new java.awt.Color(5, 153, 0));
         b_salir.setFont(new java.awt.Font("Segoe UI", 0, 20)); // NOI18N
@@ -93,10 +100,10 @@ public class VentanaPrincipal extends javax.swing.JFrame {
                 b_salirActionPerformed(evt);
             }
         });
-        getContentPane().add(b_salir, new org.netbeans.lib.awtextra.AbsoluteConstraints(870, 420, 120, 45));
+        getContentPane().add(b_salir, new org.netbeans.lib.awtextra.AbsoluteConstraints(880, 340, 120, 45));
 
         copy.setText("famaCorporation System  ©");
-        getContentPane().add(copy, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 440, 166, -1));
+        getContentPane().add(copy, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 370, 166, -1));
 
         b_email.setBackground(new java.awt.Color(5, 153, 0));
         b_email.setFont(new java.awt.Font("Segoe UI", 0, 20)); // NOI18N
@@ -109,7 +116,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
                 b_emailActionPerformed(evt);
             }
         });
-        getContentPane().add(b_email, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 180, 120, 45));
+        getContentPane().add(b_email, new org.netbeans.lib.awtextra.AbsoluteConstraints(860, 100, 120, 45));
 
         b_volver.setBackground(new java.awt.Color(5, 153, 0));
         b_volver.setFont(new java.awt.Font("Segoe UI", 0, 20)); // NOI18N
@@ -117,10 +124,52 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         b_volver.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ventanas/volver.png"))); // NOI18N
         b_volver.setText("Volver");
         b_volver.setBorder(null);
-        getContentPane().add(b_volver, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 420, 120, 45));
+        getContentPane().add(b_volver, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 340, 120, 45));
 
         l_fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ventanas/fondo_principal.jpg"))); // NOI18N
-        getContentPane().add(l_fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1010, 480));
+        getContentPane().add(l_fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -10, 1010, 410));
+
+        menubar.setBackground(new java.awt.Color(25, 153, 0));
+        menubar.setForeground(new java.awt.Color(255, 255, 255));
+
+        m_opciones.setBackground(new java.awt.Color(25, 153, 0));
+        m_opciones.setForeground(new java.awt.Color(255, 255, 255));
+        m_opciones.setText("Opciones");
+
+        opcion_abrir.setBackground(new java.awt.Color(25, 153, 0));
+        opcion_abrir.setForeground(new java.awt.Color(255, 255, 255));
+        opcion_abrir.setText("Abrir");
+        opcion_abrir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                opcion_abrirActionPerformed(evt);
+            }
+        });
+        m_opciones.add(opcion_abrir);
+
+        opcion_guardar.setBackground(new java.awt.Color(25, 153, 0));
+        opcion_guardar.setForeground(new java.awt.Color(255, 255, 255));
+        opcion_guardar.setText("Guardar");
+        m_opciones.add(opcion_guardar);
+
+        opcion_salir.setBackground(new java.awt.Color(25, 153, 0));
+        opcion_salir.setForeground(new java.awt.Color(255, 255, 255));
+        opcion_salir.setText("Salir");
+        m_opciones.add(opcion_salir);
+
+        menubar.add(m_opciones);
+
+        m_ayuda.setBackground(new java.awt.Color(25, 153, 0));
+        m_ayuda.setForeground(new java.awt.Color(255, 255, 255));
+        m_ayuda.setText("Ayuda");
+
+        acercade.setBackground(new java.awt.Color(25, 153, 0));
+        acercade.setForeground(new java.awt.Color(255, 255, 255));
+        acercade.setText("Acerca de");
+        m_ayuda.add(acercade);
+
+        menubar.add(m_ayuda);
+
+        setJMenuBar(menubar);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -141,6 +190,10 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_b_emailActionPerformed
 
+    private void opcion_abrirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_opcion_abrirActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_opcion_abrirActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -153,6 +206,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem acercade;
     private javax.swing.JButton b_almacen;
     private javax.swing.JButton b_clientes;
     private javax.swing.JButton b_email;
@@ -164,5 +218,11 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     private javax.swing.JLabel l_fondo;
     private javax.swing.JLabel l_icono;
     private javax.swing.JLabel l_text;
+    private javax.swing.JMenu m_ayuda;
+    private javax.swing.JMenu m_opciones;
+    private javax.swing.JMenuBar menubar;
+    private javax.swing.JMenuItem opcion_abrir;
+    private javax.swing.JMenuItem opcion_guardar;
+    private javax.swing.JMenuItem opcion_salir;
     // End of variables declaration//GEN-END:variables
 }
